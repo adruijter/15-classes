8cc9e0121e5ccd8a18dfe703e4a8007df72afbeb
Arjan de Ruijter <rra@mboutrecht.nl>
2014-10-28 11:38:37 +0100

    initial commit

---
+++ Classes/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Classes
+{
+    public class Player
+    {
+        //Fields
+        private string name;
+
+        //Properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        // Constructor van de class Player (heeft dezelfde naam als de class en geen returntype
+        // Dus ook geen void!
+        public Player(string name)
+        {
+            this.name = name;
+        }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player arjan, bert, harry;
+
+            // Geef aan de constructor de naam mee
+            arjan = new Player("Arjen de Ruijter");
+            //arjan.name = "Arjan de Ruijter";
+            arjan.Name = "Arjan de Ruijter";
+
+
+            bert = new Player("Bert de Vries");
+            //bert.name = "Bert de Vries";
+
+            harry = new Player("Harry de Groot");
+            //harry.name = "Harry de Groot";
+
+
+
+            Console.WriteLine("{0} heeft als score: {1}", arjan.Name, 4500);
+            Console.WriteLine("De naam opgeslagen in het object bert is: {0}", bert.Name);
+            Console.WriteLine("De naam opgeslagen in het object harry is: {0}", harry.Name);
+            Console.ReadLine();
+        }
+    }
+}
