53dd78deb5aa3dadff2e32d0173263a93f12d969
Arjan de Ruijter <rra@mboutrecht.nl>
2014-11-04 10:45:23 +0100

    Methods binnen een class

--- Classes/Player.cs
+++ Classes/Player.cs
@@ -31,5 +31,47 @@ namespace Classes
             this.name = name;
             this.score = score;
         }
+
+        public string Draw()
+        {
+            string tekst = "";
+            tekst += "Mijn naam is " + this.name + 
+                        " en ik heb in dit spel een score van: " + this.score + this.Cheer();
+            return tekst;
+        }
+
+        // < 500 => Je bent nog een Noob, >500 en < 1000 => ga zo door
+        // >1000 en < 1500 het begint er op te lijken, verzin nog twee teksten
+        private string Cheer()
+        {
+            string text = "";
+
+            if (this.score < 500)
+            {
+                text += " Conclusie: Je bent niet zo goed!";
+            }
+            else if (this.score >= 500 && this.score < 1000)
+            {
+                text += " Conclusie: Ga zo door";
+            }
+            else if (this.score >= 1000 && this.score < 1500)
+            {
+                text += " Conclusie: Het begint er op te lijken";
+            }
+            else if (this.score >= 1500 && this.score < 2000)
+            {
+                text += " Conclusie: Dit gaat prima!";
+            }
+            else if (this.score >= 2000 && this.score < 2500)
+            {
+                text += " Conclusie: Je bent een professional";
+            }
+            else
+            {
+                text += "Je behoort tot de buitencategorie";
+            }
+
+            return text;
+        }
     }
 }
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -9,10 +9,11 @@ namespace Classes
     {
         static void Main(string[] args)
         {
+            
             Player arjan, bert, harry;
 
             // Geef aan de constructor de naam mee
-            arjan = new Player("Arjen de Ruijter", 8400);
+            arjan = new Player("Arjen de Ruijter", 600);
             //arjan.name = "Arjan de Ruijter";
             arjan.Name = "Arjan de Ruijter";
 
@@ -20,7 +21,7 @@ namespace Classes
             bert = new Player("Bert de Vries", 200);
             //bert.name = "Bert de Vries";
 
-            harry = new Player("Harry de Groot", 8150);
+            harry = new Player("Harry de Groot", 1200);
             //harry.name = "Harry de Groot";
 
 
@@ -28,6 +29,10 @@ namespace Classes
             Console.WriteLine("{0} heeft als score: {1}", arjan.Name, arjan.Score);
             Console.WriteLine("{0} heeft als score: {1}", bert.Name, bert.Score);
             Console.WriteLine("{0} heeft als score: {1}", harry.Name, harry.Score);
+            Console.WriteLine(arjan.Draw());
+            Console.WriteLine(bert.Draw());
+            Console.WriteLine(harry.Draw());
+
             Console.ReadLine();
         }
     }
