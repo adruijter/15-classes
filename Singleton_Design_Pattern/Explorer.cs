71fe14b0d0fea6614b1e9071b13217d58e0be2fd
Arjan de Ruijter <rra@mboutrecht.nl>
2014-11-06 14:08:31 +0100

    Singelton Design Pattern

---
+++ Singleton_Design_Pattern/Explorer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Singleton_Design_Pattern
+{
+    public class Explorer
+    {
+        // Maak de twee private fields geschikt voor deze explorer class
+        private string favoriteWeapon;
+        private int killedMummies;
+
+        // Maak voor de twee bovenstaande fields twee properties(getter en setter)
+        public string FavoriteWeapon
+        {
+            get { return this.favoriteWeapon; }
+            set { this.favoriteWeapon = value; }
+        }
+
+        public int KilledMummies
+        {
+            get { return this.killedMummies; }
+            set { this.killedMummies = value; }
+        }
+
+        // Maak de constructor van deze class.
+        public Explorer()
+        {
+
+        }
+    }
+}
---
+++ Singleton_Design_Pattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Singleton_Design_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Maak een instantie (object) van de Explorer class.
+
+            // Stap 1. Verzin een naam van het type Explorer;
+            Explorer explorer1;
+
+            // Stap 2. Maak een nieuw object(instantie) van de class Explorer
+            explorer1 = new Explorer();
+
+            Console.WriteLine("Het favoriete wapen van deze explorer is: " + explorer1.FavoriteWeapon);
+
+
+
+            // Maak een tweede instantie (object) van de Explorer class.
+            Console.ReadLine();
+        }
+    }
+}
