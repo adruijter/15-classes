07d748fc0fe9c6704d76f9b13f09bd1fbb685c4b
Arjan de Ruijter <rra@mboutrecht.nl>
2014-11-06 15:31:10 +0100

    Opdrachten

--- OefenMetClasses/Program.cs
+++ OefenMetClasses/Program.cs
@@ -9,6 +9,31 @@ namespace OefenMetClasses
     {
         static void Main(string[] args)
         {
+            // Maak een class voor het maken van pizza's
+
+            /* Maak de volgende velden aan voor de class pizza
+             * Naam pizza
+             * Prijs pizza
+             * Manier van snijden
+             * wel of geen sauce
+             * array toppings
+             * hieropeten of takeaway
+             * temperatuur van de oven
+             * Aantal minuten kneden
+             */
+ 
+            // Maak properties voor alle Fields (get en set)
+           
+
+            // Maak een constructor die alle velden een beginwaarde kan geven (via de argumentenlijst)
+
+
+
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
