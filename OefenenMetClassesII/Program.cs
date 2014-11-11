922b6dc20e83f15261a4f7ec6bc7883c6ca1d9f2
Arjan de Ruijter <rra@mboutrecht.nl>
2014-11-11 11:16:01 +0100

    Oefenen met classesII

--- OefenenMetClassesII/Program.cs
+++ OefenenMetClassesII/Program.cs
@@ -5,10 +5,33 @@ using System.Text;
 
 namespace OefenenMetClassesII
 {
-    class Program
+    public enum sweetening { Sugar, Honey, NoCalories};
+
+    public class Program
     {
+
+
         static void Main(string[] args)
         {
+            sweetening sweet = sweetening.Honey;
+            
+            /* Maak een class Coffee
+             * Fields
+             * ++++++
+             * originBeans (country)
+             * beanSpieces (Arabica, robusta)
+             * milk (fat, low, skimmed) enum!
+             * milkWhipped
+             * sweetening (sugar, honey, noCalories) enum! 
+             * cream
+             * cookie
+             * 
+             * Maak properties voor elk field (get en set)
+             * 
+             * Maak een constructor die alle fields een beginwaarde geeft.
+             * 
+             * Maak een Draw() method die alle fieldvalues kan weergeven op het scherm.
+             */
         }
     }
 }
