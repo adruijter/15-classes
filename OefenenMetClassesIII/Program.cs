54ee252324f4311763611c39a32dfc8bf9d5d726
Arjan de Ruijter <rra@mboutrecht.nl>
2014-11-13 13:14:23 +0100

    Opdracht OefenenMetClassesIII

--- OefenenMetClassesIII/Program.cs
+++ OefenenMetClassesIII/Program.cs
@@ -5,10 +5,34 @@ using System.Text;
 
 namespace OefenenMetClassesIII
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+
+            /* Maak een class voor Zoogdieren
+             * Maak de volgende fields
+             * ++++++++++++++++++++++++
+             * aantal benen
+             * staart
+             * dag of nachtdier
+             * vleeseter/vegetarier
+             * maximale leeftijd
+             * hoeveel jongen per worp
+             * gewicht
+             * woonhabitat (land, zee)
+             * hoeveelheid in het wild aanwezig
+             * land van herkomst
+             * 
+             * 
+             * Maak voor bovenstaande fields de properties
+             * 
+             * Maak minimaal 3 constructors
+             * 
+             * Maak private hulp methods om de boolean variabele om te kunnen zetten naar tekst
+             * 
+             * Maak een Draw methode om de fields te kunnen weergeven in de console.
+
         }
     }
 }
