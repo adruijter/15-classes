33ca699fdfcb96344d3c4c6b295e8416bfcd0a89
Arjan de Ruijter <rra@mboutrecht.nl>
2014-11-13 11:37:41 +0100

    Coffee class klaar

---
+++ OefenenMetClassesII/Coffee.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OefenenMetClassesII
+{
+    
+    public class Coffee
+    {
+        // Fields
+        private Sweetening sweet = Sweetening.Honey;
+        private string originBeans = "Guatamala";
+        private BeanSpecies beanSpecies = BeanSpecies.Aribica;
+        private Milk milk = Milk.Skimmed;
+        private bool milkWhipped = true;
+        private bool cream = true;
+        private bool cookie = true;
+
+        // Properties
+        public Sweetening Sweet
+        {
+            get { return this.sweet; }
+            set { this.sweet = value;  }
+        }
+        public string OriginBeans
+        {
+            get { return this.originBeans; }
+            set { this.originBeans = value; }
+        }
+        public BeanSpecies BeanSpecies
+        {
+            get { return this.beanSpecies; }
+            set { this.beanSpecies = value; }
+        }
+        public Milk Milk
+        {
+            get { return this.milk; }
+            set { this.milk = value; }
+        }
+        public bool MilkWhipped
+        {
+            get { return this.milkWhipped; }
+            set { this.milkWhipped = value; }
+        }
+        public bool Cream
+        {
+            get { return this.cream; }
+            set { this.cream = value; }
+        }
+        public bool Cookie
+        {
+            get { return this.cookie; }
+            set { this.cookie = value; }
+        }
+
+
+        // Constructor
+        public Coffee() 
+        { 
+        
+        }
+
+        public Coffee(Sweetening sweet, string originBeans, BeanSpecies beanSpecies, Milk milk,
+                         bool milkWhipped, bool cream, bool cookie)
+        {
+            this.sweet = sweet;
+            this.originBeans = originBeans;
+            this.beanSpecies = beanSpecies;
+            this.milk = milk;
+            this.milkWhipped = milkWhipped;
+            this.cream = cream;
+            this.cookie = cookie;
+        }
+
+        private string MilkWhippedYesOrNo()
+        {
+            return ( this.milkWhipped ) ? "" : " niet";
+        }
+
+        private string MilkSort()
+        {
+            switch (this.milk)
+            {
+                case Milk.Fat:
+                    return "volle";
+                case Milk.LowFat:
+                    return "halfvolle";
+                case Milk.Skimmed:
+                    return "magere";
+                default:
+                    return "";
+            }
+        }
+
+        private string SweetSort()
+        {
+            switch (this.sweet)
+            {
+                case Sweetening.Honey:
+                    return "honing";
+                case Sweetening.NoCalories:
+                    return "een zoetje";
+                case Sweetening.Sugar:
+                    return "suiker";
+                default:
+                    return "";
+            }
+        }
+
+        private string CreamYesOrNo()
+        {
+            if (this.cream)
+            {
+                return "U krijgt slagroom op uw koffie. ";
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        private string CookieYesOrNO()
+        {
+            return (this.cookie) ? "U krijgt een lekker koekje" : "";
+        }
+
+        public void Draw()
+        {
+            System.Console.WriteLine("U heeft een kopje koffie bestelt met {0} bonen afkomstig uit {1} \n" + 
+                                     "U heeft gekozen voor {2} melk en deze is{3} opgeklopt. U gebruikt {4}\n" +
+                                     "{5}{6}", this.beanSpecies, 
+                                                this.originBeans, 
+                                                this.MilkSort(), 
+                                                this.MilkWhippedYesOrNo(),
+                                                this.SweetSort(),
+                                                this.CreamYesOrNo(),
+                                                this.CookieYesOrNO());
+        }
+    }
+}
--- OefenenMetClassesII/Program.cs
+++ OefenenMetClassesII/Program.cs
@@ -5,21 +5,59 @@ using System.Text;
 
 namespace OefenenMetClassesII
 {
-    public enum sweetening { Sugar, Honey, NoCalories};
+    
+    public enum BeanSpecies { Aribica, Robusta, Liberica };
+    public enum Milk { Fat, LowFat, Skimmed };
+    public enum Sweetening { Sugar, Honey, NoCalories };
 
     public class Program
     {
-
+        
 
         static void Main(string[] args)
         {
-            sweetening sweet = sweetening.Honey;
+            List<Coffee> koffieLijst;
+            Coffee eersteKoffie, tweedeKoffie;
+
+            koffieLijst = new List<Coffee>();
+            koffieLijst.Add(eersteKoffie = new Coffee());
+            koffieLijst.Add(tweedeKoffie = new Coffee());
+            
+            // Veranderen van koffie eigenschappen in de lijst
+            koffieLijst[0].Cream = false;
+            eersteKoffie.Cookie = false;
+
+            koffieLijst[1].MilkWhipped = false;
+            tweedeKoffie.Sweet = Sweetening.NoCalories;
+
+            // Maken van een object met een argumentloze constructor
+            Coffee koffie00 = new Coffee();
+            
+            // Maken van een object met een overladen constructor
+            Coffee koffie01 = new Coffee(Sweetening.NoCalories,
+                                         "Ethiopie",
+                                         BeanSpecies.Liberica,
+                                         Milk.LowFat,
+                                         false,
+                                         true,
+                                         false);
+            
+            // Weergeven op het scherm van de koffie objecten.
+            koffie00.Draw();
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            koffie01.Draw();
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            foreach (Coffee koffie in koffieLijst)
+            {
+                koffie.Draw();
+                Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            }
             
             /* Maak een class Coffee
              * Fields
              * ++++++
              * originBeans (country)
-             * beanSpieces (Arabica, robusta)
+             * beanSpieces (Arabica, robusta, liberica) enum!
              * milk (fat, low, skimmed) enum!
              * milkWhipped
              * sweetening (sugar, honey, noCalories) enum! 
@@ -32,6 +70,7 @@ namespace OefenenMetClassesII
              * 
              * Maak een Draw() method die alle fieldvalues kan weergeven op het scherm.
              */
+            Console.ReadLine();
         }
     }
 }
